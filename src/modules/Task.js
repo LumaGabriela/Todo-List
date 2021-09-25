6663dd54a3e9857bac473dcc14be96d0a67f7d65
LumaGabriela <lumagabriela3331@gmail.com>
2021-09-25 16:44:59 -0300

    update

--- src/modules/Task.js
+++ src/modules/Task.js
@@ -1,5 +1,5 @@
-import {currentIndex, currentProject, projectDOM, todoSelector} from './Projects'
-import { taskDOM, clearTasks , addListeners} from "./Todos";
+import {currentIndex, projectDOM, todoSelector} from './Projects'
+import { taskDOM, clearTasks, addListeners} from './Todos'
 
 
 function createTask(title, date, checked, project){
@@ -10,13 +10,13 @@ export let home = []
 export let today = []
 export let thisWeek = []
 
-export function addTodo( type,title,date,check, proj){
+export function addTodo( type, title, date, check, proj ){
     type.push(createTask(title, date, check, proj))
     populateStorage()
 }
 
 export function removeTodo(i, type){
-    type.splice(i,1)
+    type.splice(i, 1)
     populateStorage()
 }
 function createProject(title, todos){
@@ -27,7 +27,7 @@ export function addProject(title, todos){
     populateStorage()
 }
 export function removeProject(i){
-    project.splice(i,1)
+    project.splice(i, 1)
     populateStorage()
 }
 //Add methods to use objects on local storage 
@@ -45,20 +45,17 @@ Storage.prototype.getObj = function(key) {
     return JSON.parse(this.getItem(key))
 }
 //       //      //       //      
-// var currentHome = localStorage.getObj('currentHome') || [];  
-// var currentProjectObj = localStorage.getObj('currentProjectObj') || [];
-
 export function populateStorage(){
-    localStorage.setObj('currentHome', home);
-    localStorage.setObj('currentProjectObj', project);   
+    localStorage.setObj('currentHome', home)
+    localStorage.setObj('currentProjectObj', project)
     setStorageValues()
 }
 
 export function setStorageValues() {
-    var currentHome = localStorage.getObj('currentHome');  
-    var currentProjectObj = localStorage.getObj('currentProjectObj');
-    home = currentHome;
-    project = currentProjectObj;
+    var currentHome = localStorage.getObj('currentHome')
+    var currentProjectObj = localStorage.getObj('currentProjectObj')
+    home = currentHome
+    project = currentProjectObj
     clearTasks()
     if(todoSelector === 0){
         taskDOM(home)
@@ -70,15 +67,16 @@ export function setStorageValues() {
     console.log(currentProjectObj)
     console.log(currentHome) 
 }
+
 if(!localStorage.getObj('currentProjectObj')) {
-    populateStorage();
-  } else {
+    populateStorage()
+} else {
     setStorageValues()
     if(project.length > 0){
-        projectDOM();
+        projectDOM()
     }
     clearTasks() 
     addListeners(home)
-    taskDOM(home);
-  }
+    taskDOM(home)
+}
 
